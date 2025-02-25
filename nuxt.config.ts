5a550814bf8aa4a0077d9fcc71fee0fe9a0d82f5
thecodingmontana <dev.humblechris254@gmail.com>
2025-02-25 16:27:09 +0300

    feat(app): add shadcn-ui support

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,10 +7,58 @@ export default defineNuxtConfig({
     compatibilityVersion: 4,
   },
 
-  modules: ['@nuxt/eslint'],
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/image',
+    '@nuxt/icon',
+    '@nuxtjs/google-fonts',
+    '@nuxtjs/tailwindcss',
+    'shadcn-nuxt',
+    '@nuxtjs/color-mode',
+    'nuxt-og-image',
+  ],
   eslint: {
     config: {
       stylistic: true
     }
-  }
+  },
+  site: {
+    url: process.env.NUXT_PUBLIC_SITE_URL,
+    name: process.env.NUXT_PUBLIC_SITE_NAME,
+  },
+  colorMode: {
+    preference: 'system', // default value of $colorMode.preference
+    fallback: 'light', // fallback value if not system preference found
+    hid: 'nuxt-color-mode-script',
+    globalName: '__NUXT_COLOR_MODE__',
+    componentName: 'ColorScheme',
+    classPrefix: '',
+    classSuffix: '',
+    storage: 'localStorage', // or 'sessionStorage' or 'cookie'
+    storageKey: 'thecodingmontana-color-mode',
+  },
+  vite: {
+    server: {
+      allowedHosts: ['08ad-102-220-169-7.ngrok-free.app'],
+    },
+  },
+  googleFonts: {
+    families: {
+      Geist: {
+        wght: [100, 400],
+        ital: [100],
+      },
+    },
+  },
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui',
+  },
 })
