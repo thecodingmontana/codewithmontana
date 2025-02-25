2bde335cbc1817d66012606095407e174f298e26
thecodingmontana <dev.humblechris254@gmail.com>
2025-02-25 16:35:46 +0300

    chore(app): add header and footer

---
+++ layouts/default.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import Footer from '~/components/shared/Footer.vue'
+import Header from '~/components/shared/Header.vue'
+
+useHead({
+  meta: [
+    { property: 'og:title', content: `Christopher Odhiambo` },
+    {
+      name: 'description',
+      content: 'Explore Christopher\'s journey through web engineering, projects, and personal insights.',
+    },
+    {
+      property: 'og:description',
+      content: 'Explore Christopher\'s journey through web engineering, projects, and personal insights.',
+    },
+    { property: 'og:url', content: 'Explore Christopher\'s journey through web engineering, projects, and personal insights.' },
+    { name: 'twitter:title', content: 'Explore Christopher\'s journey through web engineering, projects, and personal insights.' },
+    {
+      name: 'twitter:description',
+      content: 'Explore Christopher\'s journey through web engineering, projects, and personal insights.',
+    },
+    { name: 'twitter:site', content: 'Explore Christopher\'s journey through web engineering, projects, and personal insights.' },
+  ],
+  title: `Christopher Odhiambo`,
+})
+
+defineOgImageComponent('Nuxt', {
+  headline: 'Greetings 👋',
+  title: 'Christopher Odhiambo',
+  description: 'Explore Christopher\'s journey through web engineering, projects, and personal insights.',
+})
+</script>
+
+<template>
+  <main class="min-h-screen relative overflow-hidden max-w-7xl mx-auto flex flex-col">
+    <Header />
+    <div class="flex-1">
+      <slot />
+    </div>
+    <Footer />
+  </main>
+</template>
