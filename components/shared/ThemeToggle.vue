f2f1fc29d80a989314900c6ab0c73cf720ccc475
thecodingmontana <dev.humblechris254@gmail.com>
2025-06-14 17:01:02 +0300

    fix(app): theme toggle switch

--- components/shared/ThemeToggle.vue
+++ components/shared/ThemeToggle.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { ref } from 'vue'
 import { Moon, Sun } from 'lucide-vue-next'
 import { Label } from '@/components/ui/label'
 import { Switch } from '@/components/ui/switch'
@@ -7,22 +6,18 @@ import { Switch } from '@/components/ui/switch'
 const colorMode = useColorMode()
 const checked = ref(false)
 
-const onSwitchChange = (payload: boolean) => {
-  if (payload) {
-    colorMode.value = 'light'
-  }
-  else {
-    colorMode.value = 'dark'
-  }
+const updateCheckedState = (mode: string) => {
+  checked.value = mode === 'light'
 }
 
-onBeforeMount(() => {
-  if (colorMode.value === 'light') {
-    checked.value = true
-  }
-  else {
-    checked.value = false
-  }
+watch(() => colorMode.value, (newVal) => {
+  updateCheckedState(newVal)
+}, {
+  immediate: true,
+})
+
+onMounted(() => {
+  updateCheckedState(colorMode.value)
 })
 </script>
 
@@ -33,7 +28,9 @@ onBeforeMount(() => {
         id="switch-12"
         v-model="checked"
         class="peer absolute inset-0 h-[inherit] w-auto data-[state=checked]:bg-input/50 data-[state=unchecked]:bg-input/50 [&_span]:h-full [&_span]:w-1/2 [&_span]:transition-transform [&_span]:duration-300 [&_span]:[transition-timing-function:cubic-bezier(0.16,1,0.3,1)] [&_span]:data-[state=checked]:translate-x-full [&_span]:data-[state=checked]:rtl:-translate-x-full"
-        @update:model-value="onSwitchChange"
+        @update:model-value="(payload) => {
+          colorMode.preference = payload ? 'light' : 'dark'
+        }"
       />
       <span
         class="pointer-events-none relative ms-0.5 flex min-w-8 items-center justify-center text-center peer-data-[state=checked]:text-muted-foreground/70 text-black"
@@ -57,6 +54,8 @@ onBeforeMount(() => {
     <Label
       for="switch-12"
       class="sr-only"
-    >Theme Switch</Label>
+    >
+      Theme Switch
+    </Label>
   </div>
 </template>
