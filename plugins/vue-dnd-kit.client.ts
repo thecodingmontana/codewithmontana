457b2b4b7781cb04897eb52725aed58a9d751979
thecodingmontana <dev.humblechris254@gmail.com>
2025-06-16 20:56:48 +0300

    chore(app): add test environment for the dnd

--- plugins/vue-dnd-kit.client.ts
+++ plugins/vue-dnd-kit.client.ts
@@ -2,5 +2,5 @@ import VueDnDKitPlugin from '@vue-dnd-kit/core'
 import { defineNuxtPlugin } from '#app'
 
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(VueDnDKitPlugin)
+  VueDnDKitPlugin.install(nuxtApp.vueApp)
 })
