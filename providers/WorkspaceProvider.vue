0b45c592e4978a251f9b42a32db19d5cd3c709b1
thecodingmontana <dev.humblechris254@gmail.com>
2025-06-17 10:40:32 +0300

    fix(app): resolve AddNewTask component import and missing render warning

--- providers/WorkspaceProvider.vue
+++ providers/WorkspaceProvider.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import Signout from '~/modals/global/Signout.vue'
 import AddNewProject from '~/modals/workspace/AddNewProject.vue'
+import AddNewTask from '~/modals/workspace/AddNewTask.vue'
 import MobileSidebar from '~/modals/workspace/MobileSidebar.vue'
 </script>
 
